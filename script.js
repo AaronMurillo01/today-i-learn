79e06103d58a7736aef69dfceaf3dcb1c30148f4
Aaron Murillo <amurillo11@cnm.edu>
2023-03-01 16:37:44 -0700

    adding year function to automate it self in the webpage

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ btn.addEventListener("click", function () {
     btn.textContent = "Share a fact";
   }
 });
+
+let votesInteresting = 23;
+let votesMindblowing = 5;
+const text = "Lisbon is the capital of Portugal";
+
+votesInteresting = votesInteresting + 1;
+votesInteresting++;
+console.log(votesInteresting);
+
+let totalUpvotes = votesInteresting + votesMindblowing;
+console.log("Upvotes:", totalUpvotes);
+
+let votesFalse = 4;
+const isCorrect = votesFalse < totalUpvotes;
+console.log(isCorrect);
+
+console.log(parseInt("24.533ccc"));
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+  return age;
+}
+
+const age1 = calcFactAge(2015);
+console.log(age1);
+
+console.log(calcFactAge(2020));
+console.log(calcFactAge(1990));
